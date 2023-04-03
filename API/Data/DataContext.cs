63e02fbddd8866b634c7a8c88e882e3ea99664f8
henrychris <64085210+henrychris@users.noreply.github.com>
2023-04-03 15:17:11 +0100

    switch to single project and class library

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server database
+            options.UseSqlite(_configuration.GetConnectionString("SqlConnection"));
+        }
+
+
+        public DbSet<Event> Events { get; set; }
+
+    }
+}
