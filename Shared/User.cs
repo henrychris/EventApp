33c84eb96dfea517f06c6b14a708d62ca1117e56
henrychris <64085210+henrychris@users.noreply.github.com>
2023-04-03 17:14:56 +0100

    update User class

--- Shared/User.cs
+++ Shared/User.cs
@@ -11,7 +11,9 @@ namespace Shared
 {
     public class User
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public static User NotFound = new() { };
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity), JsonIgnore]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; } = string.Empty;
@@ -19,6 +21,8 @@ namespace Shared
         public string Email { get; set; } = string.Empty;
         [Required, JsonIgnore]
         public string PasswordHash { get; set; } = string.Empty;
+        [JsonIgnore]
+        public byte[] PasswordSalt { get; set; } = new byte[32];
         public decimal WalletBalance { get; set; } = 0;
         // one user can join many events
         public virtual ICollection<Event> AttendedEvents { get; set; } = new List<Event>();
