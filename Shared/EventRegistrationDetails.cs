26db4b63e39fb859b0a34ee61c4b3dfb4566a528
henrychris <64085210+henrychris@users.noreply.github.com>
2023-04-07 11:54:55 +0100

    setup userEvents API

---
+++ Shared/EventRegistrationDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class EventRegistrationDetails
+    {
+        // setup automapper
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string EventId { get; set; }
+        public string EventName { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+        public string UserEmail { get; set; } = string.Empty;
+    }
+}
