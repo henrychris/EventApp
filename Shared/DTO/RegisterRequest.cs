148113a9a12b71796ce7b846583974f38d89ced3
henrychris <64085210+henrychris@users.noreply.github.com>
2023-04-09 15:08:17 +0100

    split user Name property into first name and last name.
    also add roles enum and default role to User

--- Shared/DTO/RegisterRequest.cs
+++ Shared/DTO/RegisterRequest.cs
@@ -5,9 +5,11 @@ namespace Shared.DTO
     public class RegisterRequest
     {
         [Required]
-        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         [Required, DataType(DataType.EmailAddress)]
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = Roles.User.ToString();
     }
 }
--- Shared/User.cs
+++ Shared/User.cs
@@ -15,9 +15,11 @@ namespace Shared
 
         [JsonIgnore]
         // remember to generate GUID when creating user
-        public int Id { get; set; }
+        public string Id { get; set; }
         [Required]
-        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
         [Required, DataType(DataType.EmailAddress)]
         public string Email { get; set; } = string.Empty;
         [Required, JsonIgnore]
@@ -25,5 +27,6 @@ namespace Shared
         [JsonIgnore]
         public byte[] PasswordSalt { get; set; } = new byte[32];
         public decimal WalletBalance { get; set; } = 0;
+        public string Role { get; set; } = Roles.User.ToString();
     }
 }
