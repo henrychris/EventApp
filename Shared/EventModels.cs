14bf7cf025e8c88b764dc48e8cf047e54da1343f
henrychris <64085210+henrychris@users.noreply.github.com>
2023-04-09 12:09:25 +0100

    moved event related classes into one file

---
+++ Shared/EventModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Event
+    {
+        public static Event NotFound = new() { };
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+    }
+
+    public class EventRegistrationDetails
+    {
+        // setup automapper
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+        [Required]
+        public string EventId { get; set; } = string.Empty;
+        public string EventName { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+        public string UserEmail { get; set; } = string.Empty;
+    }
+    public class EventDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
