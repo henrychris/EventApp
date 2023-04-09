a06d590972ab8bbff8c19f75529690eaa8def2f7
henrychris <64085210+henrychris@users.noreply.github.com>
2023-04-09 15:25:04 +0100

    increased _keySize to 128, aka 16 bytes

--- UserAPI/Services/SecurePasswordHasher.cs
+++ UserAPI/Services/SecurePasswordHasher.cs
@@ -9,7 +9,7 @@ namespace UserAPI.Services
     {
         // set these in appsettings
         // refer to EP for how to categorise settings with classes
-        const int _keySize = 64;
+        const int _keySize = 128;
         const int _iterations = 350000;
         readonly HashAlgorithmName _hashAlgorithm = HashAlgorithmName.SHA512;
 
@@ -25,7 +25,7 @@ namespace UserAPI.Services
 
             return new PasswordHash
             {
-                Hash = Convert.ToHexString(hash),
+                Hash = Convert.ToBase64String(hash),
                 Salt = salt
             };
         }
@@ -34,7 +34,7 @@ namespace UserAPI.Services
         {
             var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, _iterations, _hashAlgorithm, _keySize);
 
-            return hashToCompare.SequenceEqual(Convert.FromHexString(hash));
+            return hashToCompare.SequenceEqual(Convert.FromBase64String(hash));
         }
     }
 }
