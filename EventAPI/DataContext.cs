684748c0648a17ebde8cb9d81133e4627fb0012c
henrychris <64085210+henrychris@users.noreply.github.com>
2023-04-30 21:26:21 +0100

    set up seed for eventsDB

--- EventAPI/DataContext.cs
+++ EventAPI/DataContext.cs
@@ -15,6 +15,36 @@ namespace EventAPI
             // connect to sql server database
             options.UseSqlite(_configuration.GetConnectionString("SqlConnection"));
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>().HasData
+                (
+                new Event
+                {
+                    Id = 1,
+                    Name = "Summer Music Festival",
+                    Description = "Join us for a day of live music and fun in the sun!",
+                    Price = 50.00m,
+                    Date = new DateTime(2023, 7, 15)
+                },
+                new Event
+                {
+                    Id = 2,
+                    Name = "Summer Music Fest",
+                    Description = "Join us for a day of live music and fun in the sun!",
+                    Price = 45.00m,
+                    Date = new DateTime(2023, 7, 22)
+                },
+                new Event
+                {
+                    Id = 3,
+                    Name = "Summer Music Concert",
+                    Description = "Join us for a night of live music and fun under the stars!",
+                    Price = 55.00m,
+                    Date = new DateTime(2023, 7, 29)
+                });
+        }
         public DbSet<Event> Events { get; set; }
     }
 }
