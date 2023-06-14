50f24990f0533b5819c2e2fce6f42b07037daed1
henrychris <64085210+henrychris@users.noreply.github.com>
2023-06-14 11:46:28 +0100

    add string representation of roles

--- Shared/Roles.cs
+++ Shared/Roles.cs
@@ -6,6 +6,14 @@
         Admin
     }
 
+    public static class UserRoleStrings
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+
+        public static readonly string[] AllRoles = { Admin, User };
+    }
+
     public class Role
     {
         public int Id { get; set; }
